582281f5e4ef417aa9e9d581d8a927af2f8e0521
Thanh Danh Anthony Ngo <bosch.ngo@extaccount.com>
2021-05-11 16:23:38 +0200

    own kdtree

--- lidar_object_detection/src/quiz/cluster/kdtree.h
+++ lidar_object_detection/src/quiz/cluster/kdtree.h
@@ -2,57 +2,60 @@
 // Quiz on implementing kd tree
 
 #include "../../render/render.h"
-
+#include <sys/types.h>
+#include <vector>
 
 // Structure to represent node of kd tree
-struct Node
-{
-	std::vector<float> point;
-	int id;
-	Node* left;
-	Node* right;
+struct Node {
+  std::vector<float> point;
+  int id;
+  Node *left;
+  Node *right;
 
-	Node(std::vector<float> arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
-	{}
+  Node(std::vector<float> arr, int setId)
+      : point(arr), id(setId), left(NULL), right(NULL) {}
 
-	~Node()
-	{
-		delete left;
-		delete right;
-	}
+  ~Node() {
+    delete left;
+    delete right;
+  }
 };
 
-struct KdTree
-{
-	Node* root;
+struct KdTree {
+  Node *root;
 
-	KdTree()
-	: root(NULL)
-	{}
+  KdTree() : root(NULL) {}
 
-	~KdTree()
-	{
-		delete root;
-	}
+  ~KdTree() { delete root; }
 
-	void insert(std::vector<float> point, int id)
-	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+  void insert_helper(Node **node, uint depth, std::vector<float> point,
+                     int id) {
+    // TODO(a-ngo): deeply understand these pointers
+    // dereference to check its value
+    if (*node == NULL) {
+      *node = new Node(point, id);
+    } else {
+      // current dimension
+      uint dim = depth % 2;
 
-	}
+      if (point[dim] < (*node)->point[dim]) {
+        insert_helper(&(*node)->left, depth + 1, point, id);
+      } else {
+        insert_helper(&(*node)->right, depth + 1, point, id);
+      }
+    }
+  }
 
-	// return a list of point ids in the tree that are within distance of target
-	std::vector<int> search(std::vector<float> target, float distanceTol)
-	{
-		std::vector<int> ids;
-		return ids;
-	}
-	
+  void insert(std::vector<float> point, int id) {
+    // TODO: Fill in this function to insert a new point into the tree
+    // the function should create a new node and place correctly with in the
+    // root
+    insert_helper(&root, 0, point, id);
+  }
 
+  // return a list of point ids in the tree that are within distance of target
+  std::vector<int> search(std::vector<float> target, float distanceTol) {
+    std::vector<int> ids;
+    return ids;
+  }
 };
-
-
-
-
