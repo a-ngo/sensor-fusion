d8129cf945deb651875908ec306230ae675e504b
Thanh Danh Anthony Ngo <bosch.ngo@extaccount.com>
2021-05-27 17:58:08 +0200

    initial opencv testing

--- camera/Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
+++ camera/Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2/core.hpp>
+#include <opencv2/core/types.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp> // for canny etc.
 #include <opencv2/videoio.hpp>
@@ -11,22 +12,28 @@ void createMatrix1() {
   int nrows = 480, ncols = 640;
   cv::Mat m1_8u;
   m1_8u.create(nrows, ncols,
-               CV_8UC1); // two-channel matrix with 8bit unsigned elements
+               CV_8UC1); // two-channel matrix with 8bit unsigned char elements
   m1_8u.setTo(cv::Scalar(255)); // white
 
   // STUDENT TASK :
   // Create a variable of type cv::Mat* named m3_8u which has three channels
   // with a depth of 8bit per channel. Then, set the first channel to 255 and
   // display the result.
+  cv::Mat m3_8u;
+  m3_8u.create(nrows, ncols, CV_8UC3);
+  m3_8u.setTo(cv::Scalar(255));
 
   // show result
   string windowName = "First steps in OpenCV (m1_8u)";
   cv::namedWindow(windowName, 1); // create window
   cv::imshow(windowName, m1_8u);
-  cv::waitKey(0); // wait for keyboard input before continuing
 
   // STUDENT TASK :
   // Display the results from the STUDENT TASK above
+  string windowName2 = "First steps in OpenCV (m3_8u)";
+  cv::namedWindow(windowName2, 2); // create window
+  cv::imshow(windowName2, m3_8u);
+  cv::waitKey(0);
 }
 
 void webcam() {
@@ -44,7 +51,7 @@ void webcam() {
 }
 
 int main() {
-  // createMatrix1();
-  webcam();
+  createMatrix1();
+  // webcam(); // only for testing
   return 0;
 }
