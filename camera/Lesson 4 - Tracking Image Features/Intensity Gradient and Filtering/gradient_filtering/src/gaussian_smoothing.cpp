8edd075211b6524dcae784c225a45b3b9386358b
Thanh Danh Anthony Ngo <bosch.ngo@extaccount.com>
2021-06-04 22:20:32 +0200

    add gaussian blur

--- camera/Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/gaussian_smoothing.cpp
+++ camera/Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/gaussian_smoothing.cpp
@@ -4,27 +4,29 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-using namespace std;
-
 void gaussianSmoothing1() {
   // load image from file
   cv::Mat img;
   img = cv::imread("../images/img1gray.png");
+  cv::imshow("original_image", img);
 
   // create filter kernel
   float gauss_data[25] = {1,  4, 7, 4,  1,  4,  16, 26, 16, 4, 7, 26, 41,
                           26, 7, 4, 16, 26, 16, 4,  1,  4,  7, 4, 1};
-  cv::Mat kernel = cv::Mat(5, 5, CV_32F, gauss_data);
+  // cv::Mat kernel = cv::Mat(5, 5, CV_32F, gauss_data);
+  size_t kernel_size = 8;
+  cv::Mat kernel = cv::Mat::ones(kernel_size, kernel_size, CV_32F) /
+                   static_cast<float>(kernel_size * kernel_size);
 
   // apply filter
   cv::Mat result;
   //   cv::filter2D(img, result, -1, kernel, cv::Point(-1, -1), 0,
   //   cv::BORDER_DEFAULT);
-  cv::filter2D(img, result, -1, kernel, cv::Point(10, 10), 0,
+  cv::filter2D(img, result, -1, kernel, cv::Point(-1, -1), 0,
                cv::BORDER_DEFAULT);
 
   // show result
-  string windowName = "Gaussian Blurring";
+  std::string windowName = "Gaussian Blurring";
   cv::namedWindow(windowName, 1); // create window
   cv::imshow(windowName, result);
   cv::waitKey(0); // wait for keyboard input before continuing
