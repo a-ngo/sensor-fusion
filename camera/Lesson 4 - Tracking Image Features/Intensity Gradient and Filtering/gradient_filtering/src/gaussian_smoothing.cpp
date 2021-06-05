29abbb348a0a3a8ae16f1de4106a95150d3edb58
Thanh Danh Anthony Ngo <bosch.ngo@extaccount.com>
2021-06-05 09:47:34 +0200

    normalize

--- camera/Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/gaussian_smoothing.cpp
+++ camera/Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/gaussian_smoothing.cpp
@@ -8,15 +8,18 @@ void gaussianSmoothing1() {
   // load image from file
   cv::Mat img;
   img = cv::imread("../images/img1gray.png");
-  cv::imshow("original_image", img);
 
   // create filter kernel
   float gauss_data[25] = {1,  4, 7, 4,  1,  4,  16, 26, 16, 4, 7, 26, 41,
                           26, 7, 4, 16, 26, 16, 4,  1,  4,  7, 4, 1};
-  // cv::Mat kernel = cv::Mat(5, 5, CV_32F, gauss_data);
-  size_t kernel_size = 8;
-  cv::Mat kernel = cv::Mat::ones(kernel_size, kernel_size, CV_32F) /
-                   static_cast<float>(kernel_size * kernel_size);
+  cv::Mat kernel = cv::Mat(5, 5, CV_32F, gauss_data);
+  // size_t kernel_size = 8;
+  // cv::Mat kernel = cv::Mat::ones(kernel_size, kernel_size, CV_32F) /
+  //                  static_cast<float>(kernel_size * kernel_size);
+
+  for (int i = 0; i < 25; i++) {
+    gauss_data[i] /= 273;
+  }
 
   // apply filter
   cv::Mat result;
