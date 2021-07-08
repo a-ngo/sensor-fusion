10c6a2ff435ee3cc931d9f5556d13e5342596dcf
Thanh Danh Anthony Ngo <bosch.ngo@extaccount.com>
2021-07-08 15:16:17 +0200

    filter out ground points

--- camera/Lesson 6 - Combining Camera and Lidar/Lidar-to-Camera Point Projection/lidar_to_camera/src/show_lidar_top_view.cpp
+++ camera/Lesson 6 - Combining Camera and Lidar/Lidar-to-Camera Point Projection/lidar_to_camera/src/show_lidar_top_view.cpp
@@ -21,6 +21,12 @@ void showLidarTopview() {
 
   // plot Lidar points into image
   for (auto it = lidar_points.begin(); it != lidar_points.end(); ++it) {
+    // TASK: 2. Remove all Lidar points on the road surface while
+    // preserving measurements on the obstacles in the scene.
+    // lidar mounting position z=+1.73m
+    if ((*it).z < -1.55)
+      continue;
+
     // x facing forward and y left from sensor in m (world position)
     float xw = (*it).x;
     float yw = (*it).y;
@@ -29,25 +35,22 @@ void showLidarTopview() {
     int x =
         (-yw * image_size.width / world_size.width) + image_size.width / 2.0;
 
-    // TODO(a-ngo): 1. Change the color of the Lidar points such that
+    // TASK: 1. Change the color of the Lidar points such that
     // X=0.0m corresponds to red while X=20.0m is shown as green.
     int g = 0.0 + xw / 20.0 * 255.0;
     int r = 255.0 - xw / 20.0 * 255.0;
 
     cv::circle(top_view_img, cv::Point(x, y), 5, cv::Scalar(0, g, r), -1);
-
-    // TODO(a-ngo): 2. Remove all Lidar points on the road surface while
-    // preserving measurements on the obstacles in the scene.
   }
 
   // plot distance markers
-  float line_spacing = 2.0; // gap between distance markers
+  float line_spacing = 2.0;
   int markers_number = floor(world_size.height / line_spacing);
   for (size_t i = 0; i < markers_number; ++i) {
     int y = (-(i * line_spacing) * image_size.height / world_size.height) +
             image_size.height;
     cv::line(top_view_img, cv::Point(0, y), cv::Point(image_size.width, y),
-             cv::Scalar(255, 0, 0));
+             cv::Scalar(255, 255, 255));
   }
 
   // display image
