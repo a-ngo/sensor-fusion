724b35ace151e783be32ab13368f63527270b5d8
a-ngo <ngt7lr@LR-C-0000M.ker.de.bosch.com>
2021-08-20 10:15:58 +0200

    add initialization of states

--- kalman_filters/project/src/highway.h
+++ kalman_filters/project/src/highway.h
@@ -4,8 +4,7 @@
 #include "sensors/lidar.h"
 #include "tools.h"
 
-class Highway
-{
+class Highway {
 public:
 
 	std::vector<Car> traffic;
@@ -29,13 +28,10 @@ public:
 	int projectedSteps = 0;
 	// --------------------------------
 
-	Highway(pcl::visualization::PCLVisualizer::Ptr& viewer)
-	{
-
+	Highway(pcl::visualization::PCLVisualizer::Ptr& viewer)	{
 		tools = Tools();
 	
 		egoCar = Car(Vect3(0, 0, 0), Vect3(4, 2, 2), Color(0, 1, 0), 0, 0, 2, "egoCar");
-		
 		Car car1(Vect3(-10, 4, 0), Vect3(4, 2, 2), Color(0, 0, 1), 5, 0, 2, "car1");
 		
 		std::vector<accuation> car1_instructions;
@@ -49,8 +45,7 @@ public:
 		car1_instructions.push_back(a);
 	
 		car1.setInstructions(car1_instructions);
-		if( trackCars[0] )
-		{
+		if( trackCars[0] ) {
 			UKF ukf1;
 			car1.setUKF(ukf1);
 		}
@@ -63,8 +58,7 @@ public:
 		a = accuation(8.0*1e6, 0.0, 0.0);
 		car2_instructions.push_back(a);
 		car2.setInstructions(car2_instructions);
-		if( trackCars[1] )
-		{
+		if( trackCars[1] ) {
 			UKF ukf2;
 			car2.setUKF(ukf2);
 		}
@@ -87,8 +81,7 @@ public:
 		a = accuation(7.5*1e6, 0.0, 0.0);
 		car3_instructions.push_back(a);
 		car3.setInstructions(car3_instructions);
-		if( trackCars[2] )
-		{
+		if( trackCars[2] ) {
 			UKF ukf3;
 			car3.setUKF(ukf3);
 		}
@@ -104,11 +97,9 @@ public:
 		car3.render(viewer);
 	}
 	
-	void stepHighway(double egoVelocity, long long timestamp, int frame_per_sec, pcl::visualization::PCLVisualizer::Ptr& viewer)
-	{
+	void stepHighway(double egoVelocity, long long timestamp, int frame_per_sec, pcl::visualization::PCLVisualizer::Ptr& viewer) {
 
-		if(visualize_pcd)
-		{
+		if(visualize_pcd) {
 			pcl::PointCloud<pcl::PointXYZ>::Ptr trafficCloud = tools.loadPcd("../src/sensors/data/pcd/highway_"+std::to_string(timestamp)+".pcd");
 			renderPointCloud(viewer, trafficCloud, "trafficCloud", Color((float)184/256,(float)223/256,(float)252/256));
 		}
@@ -118,14 +109,12 @@ public:
 		renderHighway(egoVelocity*timestamp/1e6, viewer);
 		egoCar.render(viewer);
 		
-		for (int i = 0; i < traffic.size(); i++)
-		{
+		for (int i = 0; i < traffic.size(); i++) {
 			traffic[i].move((double)1/frame_per_sec, timestamp);
 			if(!visualize_pcd)
 				traffic[i].render(viewer);
 			// Sense surrounding cars with lidar and radar
-			if(trackCars[i])
-			{
+			if(trackCars[i]) {
 				VectorXd gt(4);
 				gt << traffic[i].position.x, traffic[i].position.y, traffic[i].velocity*cos(traffic[i].angle), traffic[i].velocity*sin(traffic[i].angle);
 				tools.ground_truth.push_back(gt);
@@ -139,7 +128,6 @@ public:
     			double v2 = sin(yaw)*v;
 				estimate << traffic[i].ukf.x_[0], traffic[i].ukf.x_[1], v1, v2;
 				tools.estimations.push_back(estimate);
-	
 			}
 		}
 		viewer->addText("Accuracy - RMSE:", 30, 300, 20, 1, 1, 1, "rmse");
@@ -149,32 +137,25 @@ public:
 		viewer->addText("Vx: "	+std::to_string(rmse[2]), 30, 225, 20, 1, 1, 1, "rmse_vx");
 		viewer->addText("Vy: "	+std::to_string(rmse[3]), 30, 200, 20, 1, 1, 1, "rmse_vy");
 
-		if(timestamp > 1.0e6)
-		{
-
-			if(rmse[0] > rmseThreshold[0])
-			{
+		if(timestamp > 1.0e6) {
+			if(rmse[0] > rmseThreshold[0]) {
 				rmseFailLog[0] = rmse[0];
 				pass = false;
 			}
-			if(rmse[1] > rmseThreshold[1])
-			{
+			if(rmse[1] > rmseThreshold[1]) {
 				rmseFailLog[1] = rmse[1];
 				pass = false;
 			}
-			if(rmse[2] > rmseThreshold[2])
-			{
+			if(rmse[2] > rmseThreshold[2]) {
 				rmseFailLog[2] = rmse[2];
 				pass = false;
 			}
-			if(rmse[3] > rmseThreshold[3])
-			{
+			if(rmse[3] > rmseThreshold[3]) {
 				rmseFailLog[3] = rmse[3];
 				pass = false;
 			}
 		}
-		if(!pass)
-		{
+		if(!pass) {
 			viewer->addText("RMSE Failed Threshold", 30, 150, 20, 1, 0, 0, "rmse_fail");
 			if(rmseFailLog[0] > 0)
 				viewer->addText(" X: "+std::to_string(rmseFailLog[0]), 30, 125, 20, 1, 0, 0, "rmse_fail_x");
@@ -185,7 +166,5 @@ public:
 			if(rmseFailLog[3] > 0)
 				viewer->addText("Vy: "+std::to_string(rmseFailLog[3]), 30, 50, 20, 1, 0, 0, "rmse_fail_vy");
 		}
-		
 	}
-	
 };
--- kalman_filters/project/src/ukf.cpp
+++ kalman_filters/project/src/ukf.cpp
@@ -20,11 +20,12 @@ UKF::UKF() {
   // initial covariance matrix
   P_ = MatrixXd(5, 5);
 
+  // TODO: needs to be tuned
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 3; // assuming 6 is max. rule of thumb -> use half of max
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 2; // assuming ? is max. rule of thumb -> use half of max
   
   /**
    * DO NOT MODIFY measurement noise values below.
@@ -54,6 +55,17 @@ UKF::UKF() {
    * TODO: Complete the initialization. See ukf.h for other member properties.
    * Hint: one or more values initialized above might be wildly off...
    */
+  is_initialized_ = false;
+  n_x_ = 5;
+
+  // parameters for sigma points prediction
+  n_aug_ = 7;
+  lambda_ = 3 - n_aug_; // TODO: use n_aug_ or n_x_?
+  Xsig_pred_ = MatrixXd(n_x_, 2*n_aug_+1);
+
+  weights_ = VectorXd(2*n_aug_+1);
+
+  time_us_ = 0; 
 }
 
 UKF::~UKF() {}
@@ -63,6 +75,36 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
    * TODO: Complete this function! Make sure you switch between lidar and radar
    * measurements.
    */
+  if (!is_initialized_){
+    // first measurement
+
+    // TODO: tuning with better initial parameters?
+    // initialize state
+    x_ << 0,0,0,0,0;
+
+    // initialize covariance matrices
+    P_ << 1, 0, 0, 0, 0,
+          0, 1, 0, 0, 0,
+          0, 0, 1, 0, 0,
+          0, 0, 0, 1, 0,
+          0, 0, 0, 0, 1;
+
+    is_initialized_ = true;
+
+  } else {
+
+  }
+
+  // update predicted state beliefs with the new sensor data
+  // TODO: use a flag for lidar or radar only tracking? - for testing purposes?
+
+  if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+    UpdateLidar(meas_package);
+  } else if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+    UpdateRadar(meas_package);
+  } else {
+    std::cerr << "Measurement type not supported!" << std::endl;
+  }
 }
 
 void UKF::Prediction(double delta_t) {
@@ -71,6 +113,17 @@ void UKF::Prediction(double delta_t) {
    * Modify the state vector, x_. Predict sigma points, the state, 
    * and the state covariance matrix.
    */
+  // augment state vector
+
+
+  // use delta_t to predict new sigma points
+
+
+
+
+  // make prediction of new state
+
+
 }
 
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
